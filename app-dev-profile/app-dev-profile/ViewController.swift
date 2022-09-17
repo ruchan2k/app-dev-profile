3705b39798e12348d49d67a14dfd17104fc66a92
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-17 23:22:42 +0300

    initial xcode project

---
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-dev-profile
+//
+//  Created by Ruçhan Talha Günay on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
